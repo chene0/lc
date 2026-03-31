afa6ebbf2b15f727552997b2aee3bb572f7448c2
Ethan Chen <e263chen@uwaterloo.ca>
2026-03-31 17:54:54 -0400

    missing number

---
+++ missing-number/2026-03-31.py
@@ -0,0 +1,11 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0
+
+        for num in nums:
+            res ^= num
+
+        for i in range(1, len(nums) + 1):
+            res ^= i
+
+        return res
